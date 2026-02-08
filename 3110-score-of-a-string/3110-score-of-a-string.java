ab4778f950f088b8c7adcd5f4c86e278ab697609
Ravivarmagali <vtu24851@veltech.edu.in>
2026-02-08 12:01:13 +0530

    Time: 1 ms (99.69%), Space: 43.5 MB (51.48%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,5 +1,16 @@
 class Solution {
     public int scoreOfString(String s) {
-        
+        int ans=0;
+        for(int i=0;i<s.length()-1;i++){
+            int a=i;
+            int b=i+1;
+            char r=s.charAt(a);
+            char rr=s.charAt(b);
+            int aa=r;
+            int aaa=rr;
+            int rrr=Math.abs(aa-aaa);
+            ans=ans+rrr;
+        }
+        return ans;
     }
 }
